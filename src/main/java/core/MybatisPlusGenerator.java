25e6c150930cfe3fb6dfdcd867d82a4388d76954
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-03-09 17:01:54 +0800

    20200309

---
+++ src/main/java/core/MybatisPlusGenerator.java
@@ -0,0 +1,109 @@
+package core;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+/**
+ * <p>
+ * 代码生成器
+ * </p>
+ */
+public class MybatisPlusGenerator {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //用来获取Mybatis-Plus.properties文件的配置信息
+        final ResourceBundle rb = ResourceBundle.getBundle("mybatis-plus");
+
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(rb.getString("OutputDir"));
+        gc.setFileOverride(true);
+        gc.setOpen(false);
+        gc.setEnableCache(false);
+        gc.setAuthor(rb.getString("author"));
+        gc.setKotlin(false);
+        gc.setSwagger2(false);
+        gc.setBaseResultMap(true);
+        gc.setBaseColumnList(true);
+        gc.setMapperName(rb.getString("entityName")+"Mapper");
+        gc.setXmlName(rb.getString("entityName")+"Mapper");
+        gc.setServiceName(rb.getString("entityName")+"Service");
+        gc.setServiceImplName(rb.getString("entityName")+"ServiceImpl");
+        gc.setControllerName(rb.getString("entityName")+"Controller");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setUrl(rb.getString("url"));
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername(rb.getString("userName"));
+        dsc.setPassword(rb.getString("password"));
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent(rb.getString("parent"));
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("service.impl");
+        pc.setEntity("entity");
+        pc.setMapper("mapper");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        List<FileOutConfig> focList = new ArrayList<>();
+        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return rb.getString("OutputDirXml") + "/mapper/" + tableInfo.getEntityName() + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+        mpg.setTemplate(new TemplateConfig().setXml(null));
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setSuperControllerClass("common.core.base.BaseController");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setSuperEntityClass("common.base.entity.BaseEntity");
+        strategy.setSuperMapperClass("common.core.base.BaseMapper");
+        strategy.setSuperServiceClass("common.core.base.BaseService");
+        strategy.setSuperServiceImplClass("common.core.base.BaseServiceImpl");
+        strategy.setInclude(new String[]{rb.getString("tableName")});
+        strategy.setControllerMappingHyphenStyle(true);
+//        strategy.setTablePrefix(rb.getString("prefix"));
+//        strategy.setEntityBooleanColumnRemoveIsPrefix(true);
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+}
+
+
+
