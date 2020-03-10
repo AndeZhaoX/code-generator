33cc480f668581ae51203743a422dfb4c911919d
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-03-10 11:22:15 +0800

    2020-03-10

--- src/main/java/core/MybatisPlusGenerator.java
+++ src/main/java/core/MybatisPlusGenerator.java
@@ -9,6 +9,8 @@ import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -20,89 +22,85 @@ import java.util.ResourceBundle;
  */
 public class MybatisPlusGenerator {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws IOException {
 
         //用来获取Mybatis-Plus.properties文件的配置信息
         final ResourceBundle rb = ResourceBundle.getBundle("mybatis-plus");
 
         // 代码生成器
-        AutoGenerator mpg = new AutoGenerator();
+        AutoGenerator autoGenerator = new AutoGenerator();
 
         // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir(rb.getString("OutputDir"));
-        gc.setFileOverride(true);
-        gc.setOpen(false);
-        gc.setEnableCache(false);
-        gc.setAuthor(rb.getString("author"));
-        gc.setKotlin(false);
-        gc.setSwagger2(false);
-        gc.setBaseResultMap(true);
-        gc.setBaseColumnList(true);
-        gc.setMapperName(rb.getString("entityName")+"Mapper");
-        gc.setXmlName(rb.getString("entityName")+"Mapper");
-        gc.setServiceName(rb.getString("entityName")+"Service");
-        gc.setServiceImplName(rb.getString("entityName")+"ServiceImpl");
-        gc.setControllerName(rb.getString("entityName")+"Controller");
-        gc.setEntityName(rb.getString("entityName"));
-        mpg.setGlobalConfig(gc);
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setOutputDir(rb.getString("OutputDir"));
+        globalConfig.setFileOverride(true);
+        globalConfig.setOpen(false);
+        globalConfig.setEnableCache(false);
+        globalConfig.setAuthor(rb.getString("author"));
+        globalConfig.setKotlin(false);
+        globalConfig.setSwagger2(false);
+        globalConfig.setBaseResultMap(true);
+        globalConfig.setBaseColumnList(true);
+        globalConfig.setMapperName("%sMapper");
+        globalConfig.setXmlName("%sMapper");
+        globalConfig.setServiceName("%sService");
+        globalConfig.setServiceImplName("%sServiceImpl");
+        globalConfig.setControllerName("%sController");
+        globalConfig.setEntityName("%s");
+        autoGenerator.setGlobalConfig(globalConfig);
 
         // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setDbType(DbType.MYSQL);
-        dsc.setUrl(rb.getString("url"));
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername(rb.getString("userName"));
-        dsc.setPassword(rb.getString("password"));
-        mpg.setDataSource(dsc);
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL);
+        dataSourceConfig.setUrl(rb.getString("url"));
+        dataSourceConfig.setDriverName("com.mysql.jdbc.Driver");
+        dataSourceConfig.setUsername(rb.getString("userName"));
+        dataSourceConfig.setPassword(rb.getString("password"));
+        autoGenerator.setDataSource(dataSourceConfig);
 
         // 包配置
-        PackageConfig pc = new PackageConfig();
-        pc.setParent(rb.getString("parent"));
-        pc.setController("controller");
-        pc.setService("service");
-        pc.setServiceImpl("service.impl");
-        pc.setEntity("entity");
-        pc.setMapper("mapper");
-        mpg.setPackageInfo(pc);
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setParent(rb.getString("package"));
+        packageConfig.setController("controller");
+        packageConfig.setService("service");
+        packageConfig.setServiceImpl("service.impl");
+        packageConfig.setEntity("entity");
+        packageConfig.setMapper("mapper");
+        autoGenerator.setPackageInfo(packageConfig);
 
-        // 自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                // to do nothing
-            }
-        };
-        List<FileOutConfig> focList = new ArrayList<>();
-        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                // 自定义输入文件名称
-                return rb.getString("OutputDirXml") + "/mapper/" + tableInfo.getEntityName() + StringPool.DOT_XML;
-            }
-        });
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
-        mpg.setTemplate(new TemplateConfig().setXml(null));
+        //自定义模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setController("/templates-my/controller.java.vm");
+        templateConfig.setService("/templates-my/service.java.vm");
+        templateConfig.setServiceImpl("/templates-my/serviceImpl.java.vm");
+        templateConfig.setEntity("/templates-my/entity.java.vm");
+        templateConfig.setMapper("/templates-my/mapper.java.vm");
+        templateConfig.setXml("/templates-my/mapper.xml.vm");
+        autoGenerator.setTemplate(templateConfig);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setSuperControllerClass("common.core.base.BaseController");
-        strategy.setEntityLombokModel(true);
-        strategy.setRestControllerStyle(true);
         strategy.setSuperEntityClass("common.base.entity.BaseEntity");
         strategy.setSuperMapperClass("common.core.base.BaseMapper");
         strategy.setSuperServiceClass("common.core.base.BaseService");
         strategy.setSuperServiceImplClass("common.core.base.BaseServiceImpl");
-        strategy.setInclude(new String[]{rb.getString("tableName")});
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        if (rb.containsKey("tableName")) {
+            String[] tableNames = rb.getString("tableName" ).split("," );
+            strategy.setInclude(tableNames);
+        }
         strategy.setControllerMappingHyphenStyle(true);
-//        strategy.setTablePrefix(rb.getString("prefix"));
-//        strategy.setEntityBooleanColumnRemoveIsPrefix(true);
-        mpg.setStrategy(strategy);
-        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
-        mpg.execute();
+        if (rb.containsKey("prefix")) {
+            String[] prefixes = rb.getString("prefix" ).split("," );
+            strategy.setTablePrefix(prefixes);
+            strategy.setEntityBooleanColumnRemoveIsPrefix(true);
+        }
+        autoGenerator.setStrategy(strategy);
+        autoGenerator.execute();
     }
 }
 
